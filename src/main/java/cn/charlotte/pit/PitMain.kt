d31197fcf07ab0f0898bf41425f86637a8a054b8
SharkPika <1127367472@qq.com>
2025-04-12 12:59:21 +0800

    push

---
+++ src/main/java/cn/charlotte/pit/PitMain.kt
@@ -0,0 +1,23 @@
+package cn.charlotte.pit
+
+import cn.charlotte.pit.impl.PitInternalImpl
+import cn.charlotte.pit.impl.PitInternalImpl.loaded
+import dev.meltdown.pit.Meltdown
+import org.bukkit.Bukkit
+
+object PitMain {
+    private var hook: PitHook? = null
+
+    @JvmStatic
+    fun start() {
+        Bukkit.getScheduler().runTask(ThePit.getInstance()) {
+            ThePit.getInstance().loadListener()
+            ThePit.setApi(PitInternalImpl)
+            hook = PitHook
+            hook!!.init()
+
+            Meltdown.init(ThePit.getInstance())
+            loaded = true
+        }
+    }
+}
---
+++ src/main/java/dev/meltdown/pit/Meltdown.java
@@ -0,0 +1,10 @@
+package dev.meltdown.pit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Meltdown {
+
+    public static void init(JavaPlugin plugin) {
+
+    }
+}
